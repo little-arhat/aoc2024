ea60e961d7beb5557f849fae94c8ca25113775ad
Roma Sokolov <sokolov.r.v@gmail.com>
2024-12-01 13:32:28 +0100

    dry

--- src/aoc1.cpp
+++ src/aoc1.cpp
@@ -12,20 +12,29 @@
 
 namespace r = std::ranges;
 
+
+template <typename Left, typename Right>
+void read_lines(std::ifstream& file, Left lefta, Right righta) {
+    std::string line;
+    while (std::getline(file, line)) {
+        std::istringstream ints(line);
+        int left, right;
+        ints >> left >> right;
+        lefta(left);
+        righta(right);
+    }
+}
+
+
 auto first () -> void {
     std::ifstream file("../inputs/1.txt");
     std::vector<int> lefts;
     std::vector<int> rights;
+    read_lines(file,
+        [&lefts](int left) { lefts.push_back(left); },
+        [&rights](int right) { rights.push_back(right); }
+    );
 
-    std::string line;
-    while (std::getline(file, line)) {
-        auto ints = std::istringstream{line};
-        int left;
-        int right;
-        ints >> left >> right;
-        lefts.push_back(left);
-        rights.push_back(right);
-    }
     std::sort(lefts.begin(), lefts.end());
     std::sort(rights.begin(), rights.end());
     auto zp = r::zip_view(lefts, rights);
@@ -39,16 +48,11 @@ auto second () -> void {
     std::ifstream file("../inputs/1.txt");
     std::vector<int> lefts;
     std::unordered_map<int, int> rights;
+    read_lines(file,
+        [&lefts](int left) { lefts.push_back(left); },
+        [&rights](int right) { rights[right]++; }
+    );
 
-    std::string line;
-    while (std::getline(file, line)) {
-        auto ints = std::istringstream{line};
-        int left;
-        int right;
-        ints >> left >> right;
-        lefts.push_back(left);
-        rights[right]++;
-    }
     int total = std::accumulate(lefts.begin(), lefts.end(), 0, [rights](int acc, auto&& left) {
         int matches = 0;
         auto it = rights.find(left);
