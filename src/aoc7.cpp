8316febcb4b3015977db136bfc30966c5761d77a
Roma Sokolov <sokolov.r.v@gmail.com>
2024-12-09 00:29:51 +0100

    day 7 8

---
+++ src/aoc7.cpp
@@ -0,0 +1,84 @@
+
+#include "utils.hpp"
+
+constexpr auto concat(long a, long b) -> long {
+    auto b_str = std::to_string(b);
+    return std::stol(std::to_string(a) + b_str);
+}
+
+auto second(std::string s) -> void {
+    std::ifstream file(s);
+    long sum = 0;
+    read_lines(file, [&sum](std::string line) {
+        auto colon_pos = line.find(':');
+        long num;
+        std::from_chars(line.data(), line.data() + colon_pos, num);
+        auto nums = line.substr(colon_pos + 2) | std::views::split(' ') |
+                    std::views::transform([](auto&& part) {
+                        long n;
+                        std::string s(part.begin(), part.end());
+                        std::from_chars(s.data(), s.data() + s.size(), n);
+                        return n;
+                    });
+        std::set<long> options;
+        for (auto n : nums) {
+            std::set<long> tmp;
+            for (auto o : options) {
+                tmp.insert(o + n);
+                tmp.insert(o * n);
+                tmp.insert(concat(o, n));
+            }
+            if (options.empty()) {
+                tmp.insert(n);
+            }
+            options = tmp;
+        }
+        if (options.contains(num)) {
+            sum += num;
+        }
+    });
+
+    std::println("{}", sum);
+}
+
+
+auto first(std::string s) -> void {
+    std::ifstream file(s);
+    long sum = 0;
+    read_lines(file, [&sum](std::string line) {
+        auto colon_pos = line.find(':');
+        long num;
+        std::from_chars(line.data(), line.data() + colon_pos, num);
+        auto nums = line.substr(colon_pos + 2) | std::views::split(' ') |
+                    std::views::transform([](auto&& part) {
+                        long n;
+                        std::string s(part.begin(), part.end());
+                        std::from_chars(s.data(), s.data() + s.size(), n);
+                        return n;
+                    });
+        std::set<long> options;
+        for (auto n : nums) {
+            std::set<long> tmp;
+            for (auto o : options) {
+                tmp.insert(o + n);
+                tmp.insert(o * n);
+            }
+            if (options.empty()) {
+                tmp.insert(n);
+            }
+            options = tmp;
+        }
+        if (options.contains(num)) {
+            sum += num;
+        }
+    });
+
+    std::println("{}", sum);
+}
+
+auto main(int argc, char* argv[]) -> int {
+    std::string filename = aoc(argc, argv, "../inputs/7.txt");
+    first(filename);
+    second(filename);
+    return 0;
+}
---
+++ src/aoc8.cpp
@@ -0,0 +1,137 @@
+
+#include <cmath>
+
+#include "utils.hpp"
+
+using pi = std::pair<int, int>;
+
+auto find_harmonics(pi older, pi newer, int width, int height)
+    -> std::vector<pi> {
+    auto vdiff = std::abs(older.first - newer.first);
+    auto hdiff = std::abs(older.second - newer.second);
+    auto bl_tr = older.second > newer.second;
+
+    auto newf = [&bl_tr, &vdiff, &hdiff](auto np) {
+        if (bl_tr) {
+            return std::make_pair(np.first + vdiff, np.second - hdiff);
+        } else {
+            return std::make_pair(np.first + vdiff, np.second + hdiff);
+        }
+    };
+
+    auto oldf = [&bl_tr, &vdiff, &hdiff](auto op) {
+        if (bl_tr) {
+            return std::make_pair(op.first - vdiff, op.second + hdiff);
+        } else {
+            return std::make_pair(op.first - vdiff, op.second - hdiff);
+        }
+    };
+    std::vector<pi> r;
+    auto p = newf(newer);
+    while (p.first >= 0 && p.second >= 0 && p.first < height &&
+           p.second < width) {
+        r.push_back(p);
+        p = newf(p);
+    }
+    p = oldf(older);
+    while (p.first >= 0 && p.second >= 0 && p.first < height &&
+           p.second < width) {
+        r.push_back(p);
+        p = oldf(p);
+    }
+    r.push_back(older);
+    r.push_back(newer);
+    return r;
+}
+
+auto second(std::string s) -> void {
+    std::ifstream file(s);
+
+    std::vector<std::vector<char>> g;
+
+    read_lines(file, [&g](std::string line) {
+        g.emplace_back(line.begin(), line.end());
+    });
+    int w = g[0].size();
+    int h = g.size();
+
+    std::unordered_set<pi, pair_hash> nodes;
+    std::unordered_map<char, std::unordered_set<pi, pair_hash>> antennas;
+    int y = 0;
+    for (auto row : g) {
+        int x = 0;
+        for (auto ch : row) {
+            pi newer = {y, x};
+            if (ch != '.') {
+                for (auto older : antennas[ch]) {
+                    auto a = find_harmonics(older, newer, w, h);
+                    nodes.insert(a.begin(), a.end());
+                }
+                antennas[ch].insert(newer);
+            }
+
+            x++;
+        }
+        y++;
+    }
+
+    std::println("{}", nodes.size());
+}
+
+auto find_antinodes(pi older, pi newer) -> std::pair<pi, pi> {
+    auto vdiff = std::abs(older.first - newer.first);
+    auto hdiff = std::abs(older.second - newer.second);
+    // y coord is always same or greater
+    if (older.second > newer.second) {
+        // bottom left -> top right
+        return {{older.first - vdiff, older.second + hdiff},
+                {newer.first + vdiff, newer.second - hdiff}};
+
+    } else {
+        // top left -> bottom right
+        return {{newer.first + vdiff, newer.second + hdiff},
+                {older.first - vdiff, older.second - hdiff}};
+    }
+}
+
+auto first(std::string s) -> void {
+    std::ifstream file(s);
+
+    std::unordered_set<pi, pair_hash> nodes;
+    std::unordered_map<char, std::unordered_set<pi, pair_hash>> antennas;
+
+    int w = -1;
+    int h = 0;
+
+    read_lines(file, [&nodes, &antennas, &h, &w](std::string line) {
+        int c = 0;
+        for (auto ch : line) {
+            pi newer = {h, c};
+            if (ch != '.') {
+                for (auto older : antennas[ch]) {
+                    auto [a, b] = find_antinodes(older, newer);
+                    nodes.insert(a);
+                    nodes.insert(b);
+                }
+                antennas[ch].insert(newer);
+            }
+            c++;
+        }
+
+        h++;
+        w = line.size();
+    });
+
+    auto results = std::ranges::to<std::unordered_set<pi, pair_hash>>(
+        nodes | std::views::filter([&h, &w](const auto& p) {
+            return p.first >= 0 && p.second >= 0 && p.first < h && p.second < w;
+        }));
+    std::println("{}", results.size());
+}
+
+auto main(int argc, char* argv[]) -> int {
+    std::string filename = aoc(argc, argv, "../inputs/8.txt");
+    // first(filename);
+    second(filename);
+    return 0;
+}
